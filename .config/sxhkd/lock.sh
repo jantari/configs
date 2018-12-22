2d6cb277b773108a7e0d0fe4aefe462e6419eaee
jantari <jantari@live.com>
2018-12-22 15:14:43 +0100

    further improved lock script

--- .config/sxhkd/lock.sh
+++ .config/sxhkd/lock.sh
@@ -13,21 +13,22 @@ else
         -gravity center \
         -pointsize 160 \
         -font FontAwesome \
-        -fill $color7 \
+        -fill $color6 \
         -stroke $color0 \
-        -strokewidth 4 \
-        -annotate +0+100 '' ~/.config/sxhkd/lock.png && \
+        -strokewidth 3 \
+        -annotate 5x5+5-24 '' ~/.config/sxhkd/lock.png && \
     i3lock \
         --image="/home/jantari/.config/sxhkd/lock.png" \
-        --clock \
         --timestr="%H:%M" \
+        --insidevercolor=00000000 \
+        --insidecolor=00000000 \
         --datecolor=00000000 \
         --timecolor=FFFFFFFF \
-        #--timepos="x+155:y+624" \
-        #--indpos="x+50:y+614" \
-        --radius=25 \
+        --line-uses-ring \
+        --radius=10\
         --veriftext='' \
-        --wrongtext=''
+        --wrongtext='' \
+        #--clock
 fi
 
 #-draw "circle 2000,720 2000,760" \
