0d2df8c84da886d926bb43e18df324d1f10e46df
jantari <jantari@live.com>
2018-12-22 14:30:22 +0100

    updated sxhkdrc and lock script

--- .config/sxhkd/lock.sh
+++ .config/sxhkd/lock.sh
@@ -1,1 +1,34 @@
-. "${HOME}/.cache/wal/colors.sh" && i3lock --blur 7 --clock --wrongtext="" --ringcolor="${color0}A0" --insidecolor="${color2}D0" --linecolor="#00000000" --indicator --timestr="%H:%M" --veriftext="" --insidewrongcolor="#FFFFFF00" --ringwrongcolor="${color3}FF" --insidevercolor="#FFFFFF00" --ringvercolor="#00000055"
+
+source ~/.cache/wal/colors.sh
+
+if [ "$1" = "blur" ];
+    then
+    # Blur effect
+    i3lock --blur 5
+else
+    # Pixelate effect
+    maim -u | convert png:- \
+        -scale 20% \
+        -scale 500% \
+        -gravity center \
+        -pointsize 160 \
+        -font FontAwesome \
+        -fill $color7 \
+        -stroke $color0 \
+        -strokewidth 4 \
+        -annotate +0+100 '' ~/Pictures/lock.png && \
+    i3lock \
+        --image="/home/jantari/Pictures/lock.png" \
+        --clock \
+        --timestr="%H:%M" \
+        --datecolor=00000000 \
+        --timecolor=FFFFFFFF \
+        #--timepos="x+155:y+624" \
+        #--indpos="x+50:y+614" \
+        --radius=25 \
+        --veriftext='' \
+        --wrongtext=''
+fi
+
+#-draw "circle 2000,720 2000,760" \
+
