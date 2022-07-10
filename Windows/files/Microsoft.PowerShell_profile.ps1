c1aa1502a2f48382e43bc2068b2bbb1f63b80dba
jantari <jantari@github.com>
2022-07-10 22:37:36 +0200

    add powershell profile

---
+++ Windows/files/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineOption -PredictionSource History
+
+if ($PSStyle) {
+    $PSStyle.FileInfo.Directory = "`e[4;1m"
+}
+
+Invoke-Expression (&starship init powershell)
