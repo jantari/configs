943e981e3f9602b23cac7068916c8ec214d4dec2
jantari <jantari@github.com>
2022-10-10 21:35:42 +0200

    start a bootstrap / setup script for windows, using ansible OOTB is just not practical

---
+++ Windows/initial-setup.ps1
@@ -0,0 +1,24 @@
+﻿# Install scoop
+if (-not (Get-Command scoop -ErrorAction Ignore)) {
+    Invoke-RestMethod get.scoop.sh | Invoke-Expression
+}
+
+scoop bucket add extras
+
+$ScoopPackages = @(
+    'starship',
+    'neovim',
+    'jq',
+    'git',
+    'delta',
+    'tokei',
+    'sed',
+    'bottom',
+    'bat'
+)
+
+scoop install @ScoopPackages
+
+# Install Windows Terminal Preview from Microsoft Store
+winget install --source msstore --id "9N8G5RFZ9XK3" --accept-source-agreements --accept-package-agreements --silent
+
