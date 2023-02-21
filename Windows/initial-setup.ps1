d3d69406d46d313e2930ecf2e7226e14dfed9931
jantari <jantari@github.com>
2023-02-21 22:14:54 +0100

    add more software + wsl2 to windows setup script

--- Windows/initial-setup.ps1
+++ Windows/initial-setup.ps1
@@ -1,4 +1,14 @@
-﻿# Install scoop
+﻿# Install some software as non-portable versions
+@(
+    '7zip.7zip',
+    'SumatraPDF.SumatraPDF',
+    'VideoLAN.VLC',
+    'Mozilla.Firefox'
+) | ForEach-Object {
+    winget install --source winget --id "$_" --scope machine --silent
+}
+
+# Install scoop
 if (-not (Get-Command scoop -ErrorAction Ignore)) {
     Invoke-RestMethod get.scoop.sh | Invoke-Expression
 }
@@ -17,11 +27,29 @@ $ScoopPackages = @(
     'tokei',
     'sed',
     'bottom',
-    'bat'
+    'bat',
+    'gping'
 )
 
 scoop install @ScoopPackages
 
 # Install Windows Terminal Preview from Microsoft Store
 winget install --source msstore --id "9N8G5RFZ9XK3" --accept-source-agreements --accept-package-agreements --silent
+
+# Begin As Administrator
+#
+# Set up WSL2
+# https://learn.microsoft.com/en-us/windows/wsl/install
+Start-Process -FilePath powershell.exe -Verb RunAs -Wait -ArgumentList @(
+    '-NoProfile',
+    '-Command',
+    '"wsl --install --no-distribution"'
+)
+
+wsl --version
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "WSL is not set up properly (or outdated). Retry after a reboot."
+}
+#
+# End As Administrator
 
