c3318649239d656b7dcb805dbe64cd147c147118
jantari <jantari@github.com>
2023-11-22 02:15:27 +0100

    working v1 of adapting elevated co-process for setup script
    also implement a really hacky synchronous command run option,
    which is needed in cases where the non-elevated process depends
    on an elevated action to complete before it can continue.

--- Windows/initial-setup.ps1
+++ Windows/initial-setup.ps1
@@ -2,20 +2,273 @@
 # Switching from Unrestricted is also required for the scoop setup.
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-Start-Process -FilePath powershell.exe -Verb RunAs -Wait -ArgumentList @(
-    '-NoProfile',
-    '-Command',
-    "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force"
-)
+# New unique pipe name every time to avoid conflicts
+[string]$PipeName = 'psipc-' + (New-Guid).Guid
+[string]$ElevatedClient = @'
+[string]$PipeName = '{0}'
+'@ -f $PipeName + @'
+
+# Fail early on all possible errors and inform the parent
+Set-StrictMode -Version 2.0
+$ErrorActionPreference = 'Stop'
+$global:ErrorActionPreference = 'Stop'
+
+trap {
+    Write-Host "$_"
+    #$ss.WriteString("ERROR");
+    &pause
+    break
+}
+
+Write-Host "GOT PARAMETER: $PipeName"
+
+class StreamString {
+    [System.IO.Stream]$ioStream
+    [System.Text.UTF8Encoding]$streamEncoding
+
+    [PSObject] ReadString() {
+        [int]$len = 0;
+        $CmdId = $this.ioStream.ReadByte()
+        $flags = $this.ioStream.ReadByte()
+        $len = $this.ioStream.ReadByte()
+        if ($len -eq -1) {
+            return $null
+        }
+        $len = $len * 256
+        $len += $this.ioStream.ReadByte()
+        $inBuffer = [Byte[]]::new($len)
+        $this.ioStream.Read($inBuffer, 0, $len)
+        return [PSCustomObject]@{
+            'CommandId' = $CmdId
+            'RunSync'   = [bool]($flags -band 1)
+            'Command'   = $this.streamEncoding.GetString($inBuffer)
+        }
+    }
+
+    [int] WriteString([string]$outString) {
+        [byte[]]$outBuffer = $this.streamEncoding.GetBytes($outString)
+        [int]$len = $outBuffer.Length
+        if ($len -gt [UInt16]::MaxValue) {
+            $len = [UInt16]::MaxValue
+        }
+        $this.ioStream.WriteByte([byte][Math]::Floor($len / 256))
+        $this.ioStream.WriteByte([byte]($len -band 255))
+        $this.ioStream.Write($outBuffer, 0, $len)
+        $this.ioStream.Flush();
+        return $outBuffer.Length + 2;
+    }
+}
+
+$pipeClient = [System.IO.Pipes.NamedPipeClientStream]::new(".", $PipeName, 'InOut', 'None')
+Write-Host "Connecting back to parent process/server..."
+$pipeClient.Connect();
+Write-Host 'Connected!'
+
+$CommandQueue     = [System.Collections.Queue]::Synchronized( [System.Collections.Queue]::new() )
+$CommandRunningHT = [Hashtable]::Synchronized( @{'CommandRunning' = $false } )
+$TSPipeClient     = [System.IO.Stream]::Synchronized($pipeClient)
+
+$ss = [StreamString]@{
+    'ioStream' = $TSPipeClient
+    'streamEncoding' = [System.Text.UnicodeEncoding]::UTF8
+}
+
+
+
+$runspace  = [runspacefactory]::CreateRunspace()
+$runspace.Open()
+
+$runspace.SessionStateProxy.SetVariable('CommandQueue',   $CommandQueue)
+$runspace.SessionStateProxy.SetVariable('CommandRunningHT', $CommandRunningHT)
+$runspace.SessionStateProxy.SetVariable('TSPipeClient',   $TSPipeClient)
+
+$powershell = [powershell]::Create()
+$powershell.Runspace = $runspace
+
+$powershell.AddScript({
+    class StreamString {
+        [System.IO.Stream]$ioStream
+        [System.Text.UTF8Encoding]$streamEncoding
+
+        [PSObject] ReadString() {
+            [int]$len = 0;
+            $CmdId = $this.ioStream.ReadByte()
+            $flags = $this.ioStream.ReadByte()
+            $len = $this.ioStream.ReadByte()
+            if ($len -eq -1) {
+                return $null
+            }
+            $len = $len * 256
+            $len += $this.ioStream.ReadByte()
+            $inBuffer = [Byte[]]::new($len)
+            $this.ioStream.Read($inBuffer, 0, $len)
+            return [PSCustomObject]@{
+                'CommandId' = $CmdId
+                'RunSync'   = [bool]($flags -band 1)
+                'Command'   = $this.streamEncoding.GetString($inBuffer)
+            }
+        }
+
+        [int] WriteString([string]$outString) {
+            [byte[]]$outBuffer = $this.streamEncoding.GetBytes($outString)
+            [int]$len = $outBuffer.Length
+            if ($len -gt [UInt16]::MaxValue) {
+                $len = [UInt16]::MaxValue
+            }
+            $this.ioStream.WriteByte([byte][Math]::Floor($len / 256))
+            $this.ioStream.WriteByte([byte]($len -band 255))
+            $this.ioStream.Write($outBuffer, 0, $len)
+            $this.ioStream.Flush();
+            return $outBuffer.Length + 2;
+        }
+    }
+
+    $ss = [StreamString]@{
+        'ioStream' = $TSPipeClient
+        'streamEncoding' = [System.Text.UnicodeEncoding]::UTF8
+    }
+
+    $ss.WriteString("READY_FOR_INSTRUCTIONS")
+
+    do {
+        $IncomingCmd = $ss.ReadString()
+        $CommandQueue.Enqueue($IncomingCmd)
+        if ($IncomingCmd.RunSync) {
+            $ss.WriteString("ENQUEUED $($IncomingCmd.CommandId) (SYNC)")
+            # Wait and dont enqueue more commands until the just-queued
+            # synchronous command has finished (aka the queue is empty)
+            # because if we loop around again and wait for more commands
+            # to queue, ReadString() blocks the pipe on our end and we
+            # can't write to it to communicate the exec status of the sync
+            # command back to our parent process.
+            do {
+                Start-Sleep -Seconds 1
+            } until ($CommandQueue.Count -eq 0 -and $CommandRunningHT['CommandRunning'] -eq $false)
+        }
+    } while (1)
+}).AddArgument($PipeName) | Out-Null
+
+$CommandRunspace = $powershell.BeginInvoke()
+
+$LastReportedQueueSize = -1
+$WorkerRunspace = [powershell]::Create()
+$Handle = $null
+
+do {
+    if ($CommandQueue.Count -ne $LastReportedQueueSize) {
+        Write-Host "[$(Get-Date -Format 'HH:mm:ss')] $($CommandQueue.Count) commands waiting in the queue" -ForegroundColor Yellow
+        $LastReportedQueueSize = $CommandQueue.Count
+    }
+
+    if ($CommandQueue.Count -and $WorkerRunspace.Runspace.RunspaceAvailability -eq 'Available') {
+        Write-Host "Executing next command ..." -ForegroundColor Yellow
+        $CommandRunningHT['CommandRunning'] = $true
+        $CmdData = $CommandQueue.Dequeue()
+        $null   = $WorkerRunspace.AddScript( [ScriptBlock]::Create($CmdData.Command) )
+        $Handle = $WorkerRunspace.BeginInvoke()
+        if ($CmdData.RunSync) {
+            $null = $ss.WriteString("STARTED $($CmdData.CommandId)");
+        }
+    }
+
+    Start-Sleep -Seconds 2
+
+    if ($Handle -and $Handle.IsCompleted) {
+        Write-Host "Calling EndInvoke() for a completed command ..." -ForegroundColor Yellow
+        $WorkerRunspace.EndInvoke($handle)
+        $Handle = $null
+        if ($CmdData.RunSync) {
+            $null = $ss.WriteString("FINISHED $($CmdData.CommandId)");
+        }
+        $CommandRunningHT['CommandRunning'] = $false
+    }
+} while (1)
+'@
+
+[string]$ElevatedClientB64 = [System.Convert]::ToBase64String( [System.Text.Encoding]::Unicode.GetBytes($ElevatedClient) )
+Start-Process -FilePath powershell.exe -Verb RunAs -ArgumentList "-NoExit -NonInteractive -NoProfile -EncodedCommand $ElevatedClientB64"
+
+class StreamString {
+    [System.IO.Stream]$ioStream
+    [System.Text.UTF8Encoding]$streamEncoding
+
+    [PSObject] ReadString() {
+        [int]$len = 0;
+        $len = $this.ioStream.ReadByte()
+        if ($len -eq -1) {
+            return $null
+        }
+        $len = $len * 256
+        $len += $this.ioStream.ReadByte()
+        $inBuffer = [Byte[]]::new($len)
+        $this.ioStream.Read($inBuffer, 0, $len)
+        return $this.streamEncoding.GetString($inBuffer)
+    }
+
+    [int] WriteString([string]$outString) {
+        [byte[]]$outBuffer = $this.streamEncoding.GetBytes($outString)
+        [int]$len = $outBuffer.Length
+        if ($len -gt [UInt16]::MaxValue) {
+            $len = [UInt16]::MaxValue
+        }
+        $this.ioStream.WriteByte([byte]0) # CommandId, doesn't matter if we're not waiting on it
+        $this.ioStream.WriteByte([byte]0) # 0b00000000 as the flag for "run this asynchronously"
+        $this.ioStream.WriteByte([byte][Math]::Floor($len / 256))
+        $this.ioStream.WriteByte([byte]($len -band 255))
+        $this.ioStream.Write($outBuffer, 0, $len)
+        $this.ioStream.Flush();
+        return $outBuffer.Length + 2;
+    }
+
+    [PSObject] EnqueueCommandAndWait([byte]$CommandId, [string]$outString) {
+        [byte[]]$outBuffer = $this.streamEncoding.GetBytes($outString)
+        [int]$len = $outBuffer.Length
+        if ($len -gt [UInt16]::MaxValue) {
+            $len = [UInt16]::MaxValue
+        }
+        $this.ioStream.WriteByte($CommandId) # CommandId, so we know what to wait for
+        $this.ioStream.WriteByte([byte]1) # 0b00000001 as the flag for "run this synchronously"
+        $this.ioStream.WriteByte([byte][Math]::Floor($len / 256))
+        $this.ioStream.WriteByte([byte]($len -band 255))
+        $this.ioStream.Write($outBuffer, 0, $len)
+        $this.ioStream.Flush();
+
+        $read = ''
+        do {
+            Write-Host "Blocking in read from pipe (getting exec status) ..."
+            $read = $this.ReadString()
+            Write-Host "[CLIENT] $read"
+        } until ($read -eq "FINISHED $CommandId")
+
+        return $outBuffer.Length + 4;
+    }
+}
+
+$namedPipeServer = [System.IO.Pipes.NamedPipeServerStream]::new($PipeName, 'InOut', 1, 'Byte');
+
+$task = $namedPipeServer.WaitForConnectionAsync()
+do {
+    Write-Host "Waiting for pipe connection"
+} until ($task.Wait([timespan]::FromSeconds(2)))
+
+$ss = [StreamString]@{
+    'ioStream' = $namedPipeServer
+    'streamEncoding' = [System.Text.UnicodeEncoding]::UTF8
+}
+
+Write-Host "Reading first line from child process:"
+do {
+    $read = $ss.ReadString()
+    Write-Host "[CLIENT] $read"
+} until ($read -eq 'READY_FOR_INSTRUCTIONS')
+
+# Send commands!
+$ss.EnqueueCommandAndWait(42, 'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force') | Out-Host
 
 Install-Module -Name PSReadLine -Scope CurrentUser -Force
 
 # Update all Microsoft Store apps (async)
-Start-Process -FilePath powershell.exe -Verb RunAs -Wait -ArgumentList @(
-    '-NoProfile',
-    '-Command',
-    '"Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName "UpdateScanMethod""'
-)
+$ss.WriteString('Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName "UpdateScanMethod"') | Out-Host
 
 # Settings
 ## Disable notifications from Ad / spam apps
@@ -48,11 +301,20 @@ reg.exe add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d 
 # Wait for winget command to be come available (may need to wait for the "Microsoft.DesktopAppInstaller" app to update through the Store first)
 # TODO: periodically re-run the store update CIM method because the store itself updating can sometimes interrupt and stop the updating process of other apps
 Write-Host "Waiting for winget to become available ..."
+$WaitTimer = [System.Diagnostics.Stopwatch]::StartNew()
+[TimeSpan]$LastInitiatedStoreUpdate = [TimeSpan]::Zero
 do {
     $DAIVersion = (Get-AppxPackage -Name Microsoft.DesktopAppInstaller).Version
     $WingetCmd  = Get-Command -Name winget -CommandType Application -ErrorAction Ignore
-    Write-Host "[$(Get-Date -Format 'HH:mm:ss')] winget available: $( if ($WingetCmd) { "True" } else { "False" } ), DesktopAppInstaller version: $DAIVersion"
+    Write-Host "[$(Get-Date -Format 'HH:mm:ss') - $($WaitTimer.Elapsed)] winget available: $( if ($WingetCmd) { "True" } else { "False" } ), DesktopAppInstaller version: $DAIVersion"
     Start-Sleep -Seconds 10
+    # Re-initiate Store updates every 5 minues until winget is available
+    if ($WaitTimer.Elapsed - $LastInitiatedStoreUpdate -ge [TimeSpan]::FromMinutes(5)) {
+        # Update all Microsoft Store apps (async)
+        Write-Host "[$(Get-Date -Format 'HH:mm:ss') - $($WaitTimer.Elapsed)] Re-initializing Store app updates"
+        $ss.WriteString('Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName "UpdateScanMethod"') | Out-Host
+        $LastInitiatedStoreUpdate = $WaitTimer.Elapsed
+    }
 } until ($WingetCmd)
 
 # Install some software as non-portable versions
@@ -66,7 +328,7 @@ do {
     'Microsoft.PowerShell',
     'Microsoft.PowerShell.Preview'
 ) | ForEach-Object {
-    winget install --source winget --id "$_" --scope machine --silent --no-upgrade
+    $null = $ss.WriteString("winget install --source winget --id `"$_`" --scope machine --silent --no-upgrade")
 }
 
 # Install scoop
@@ -173,11 +435,7 @@ winget install --source msstore --id "9NCBCSZSJRSB"  --accept-source-agreements 
 #
 # Set up WSL2
 # https://learn.microsoft.com/en-us/windows/wsl/install
-Start-Process -FilePath powershell.exe -Verb RunAs -Wait -ArgumentList @(
-    '-NoProfile',
-    '-Command',
-    '"wsl --install --no-distribution"'
-)
+$ss.EnqueueCommandAndWait(43, 'wsl --install --no-distribution') | Out-Host
 
 wsl --version
 if ($LASTEXITCODE -ne 0) {
